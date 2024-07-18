3a186ce44cdf6e42a1a252454e9684e0da0f4e9d
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2024-07-19 08:24:57 +1000

    added the remaining tests

---
+++ Solution/Tests/Tests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using Application;
+using AutoFixture;
+using Tests.Common;
+using Xunit;
+
+namespace Tests;
+
+public class SolutionTests : CommonTestsFixture
+{
+    [Fact]
+    public void Method_ShouldDoSomething()
+    {
+        var sut = FixtureData.Create<Solution>();
+
+        //do something
+
+        //assert
+    }
+}
