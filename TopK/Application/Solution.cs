619f9899441be7d5dda1e20027da5d2c60831676
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2024-07-22 08:22:57 +1000

    implemented Top K solution

---
+++ TopK/Application/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace Application;
+
+public class Solution
+{
+    private readonly Dictionary<int, int> inputNumbers = new();
+    private PriorityQueue<int, int> priorityQueue; //min heap
+
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var output = new int[k];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            //1,1,1,2,2,3
+            var currentInput = nums[i]; //1
+
+            if (!inputNumbers.ContainsKey(currentInput)) inputNumbers.Add(currentInput, 0);
+
+            inputNumbers[currentInput]++;
+        }
+
+        //{1:3, 2:2, 3:1}
+        var inputOrdered = new List<(int, int)>();
+        foreach (var key in inputNumbers.Keys)
+        {
+            var value = inputNumbers[key];
+            inputOrdered.Add(new ValueTuple<int, int>(key, value));
+        }
+
+        priorityQueue = new PriorityQueue<int, int>(inputOrdered, new InputComparer());
+
+        for (var i = 0; i < k; i++) output[i] = priorityQueue.Dequeue();
+
+        return output;
+    }
+}
+
+public class InputComparer : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        return y.CompareTo(x);
+    }
+}
---
+++ TopK/Tests/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application;
+using AutoFixture;
+using FluentAssertions;
+using Tests.Common;
+using Xunit;
+
+namespace Tests
+{
+    public class SolutionTests : CommonTestsFixture
+    {
+        [Theory]
+        [ClassData(typeof(InputWithOutputs))]
+        public void TopKFrequent_ShouldReturnExpectedResults(int[] input, int topK, int[] output)
+        {
+            var sut = FixtureData.Create<Solution>();
+
+            var result = sut.TopKFrequent(input, topK);
+
+            result.Should().BeEquivalentTo(output, config => config.WithStrictOrdering());
+        }
+    }
+}
