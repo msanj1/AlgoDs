dd6055b832dc8926d4eb067abd93c4b41f737335
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2024-10-02 08:13:26 +1000

    solved the climbing stairs problem

---
+++ ClimbingStairs/ClimbingStairs/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace ClimbingStairs
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            var steps = new int[n + 1];
+            steps[steps.Length - 1] = 1;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                var distinctWaysToClimb = 0;
+                var currentStep = i;
+
+                var next1Step = currentStep + 1;
+
+                var next2Steps = currentStep + 2;
+
+                if (next1Step <= n)
+                {
+                    distinctWaysToClimb += steps[next1Step];
+                }
+
+                if (next2Steps <= n)
+                {
+                    distinctWaysToClimb += steps[next2Steps];
+                }
+
+                steps[i] = distinctWaysToClimb;
+            }
+
+            return steps[0];
+        }
+    }
+
+}
---
+++ ClimbingStairs/Tests/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using ClimbingStairs;
+using FluentAssertions;
+using Tests.Common;
+using Xunit;
+
+namespace Tests;
+
+public class SolutionTests : CommonTestsFixture
+{
+    [Fact]
+    public void ClimbStairs_ShouldReturnCorrectSteps()
+    {
+        var sut = FixtureData.Create<Solution>();
+
+        //do something
+        var result = sut.ClimbStairs(2);
+
+        //assert
+        result.Should().Be(2);
+    }
+}
