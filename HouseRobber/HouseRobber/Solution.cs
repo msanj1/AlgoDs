721b7b4ca24905c2b534280466054cd608ce79b1
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2024-10-21 07:25:41 +1000

    Implemented the House Robber challenge

---
+++ HouseRobber/HouseRobber/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace ClimbingStairs
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return -1;
+            }
+
+            int previousPreviousMax = 0;
+            int previousMax = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            { //[1,1,3,*3]
+                var currentMoneyInHouse = nums[i];
+
+                var temp = previousMax;
+
+                previousMax = Math.Max(currentMoneyInHouse + previousPreviousMax, previousMax);
+                previousPreviousMax = temp;
+
+            }
+
+            return previousMax;
+        }
+    }
+}
---
+++ HouseRobber/Tests/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using ClimbingStairs;
+using FluentAssertions;
+using Tests.Common;
+using Xunit;
+
+namespace Tests;
+
+public class SolutionTests : CommonTestsFixture
+{
+    [Fact]
+    public void ClimbStairs_ShouldReturnCorrectSteps()
+    {
+        var sut = FixtureData.Create<Solution>();
+
+        //do something
+        var result = sut.Rob(new int[] { 1,2,3,1 });
+
+        //assert
+        result.Should().Be(4);
+    }
+}
