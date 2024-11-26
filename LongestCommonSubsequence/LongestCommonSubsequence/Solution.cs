bc87f56696ba30504a35190db1f1de2b16cf17e8
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2024-11-27 07:11:54 +1000

    Implemented the Longest Common Subsequence challenge

---
+++ LongestCommonSubsequence/LongestCommonSubsequence/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LongestCommonSubsequence
+{
+    public class Solution
+    {
+        public int LongestCommonSubsequence(string text1, string text2) //Time and Space Complexity O(T*C)
+        {
+            var calculations = new int[text2.Length + 1][];
+
+            for (int i = 0; i < calculations.Length; i++)
+            {
+                calculations[i] = new int[text1.Length + 1];
+            }
+
+            for (int row = calculations.Length - 2; row >= 0; row--)
+            {
+                for (int col = calculations[row].Length - 2; col >= 0; col--)
+                {
+                    if (text2[row] == text1[col])
+                    {
+                        calculations[row][col] = 1 + calculations[row + 1][col + 1];
+                    }
+                    else
+                    {
+                        calculations[row][col] = Math.Max(calculations[row + 1][col], calculations[row][col + 1]);
+                    }
+                }
+            }
+
+            return calculations[0][0];
+        }
+    }
+
+}
---
+++ LongestCommonSubsequence/Tests/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using LongestCommonSubsequence;
+using Tests.Common;
+using Xunit;
+
+namespace Tests;
+
+public class SolutionTests : CommonTestsFixture
+{
+    [Theory]
+    [InlineData("cat", "crabt", 3)]
+    [InlineData("abcd", "abcd", 4)]
+    public void LongestCommonSubsequence_ShouldReturnExpectedValue(string text1, string text2, int expectedValue)
+    {
+        var sut = FixtureData.Create<Solution>();
+
+        //do something
+        var result = sut.LongestCommonSubsequence(text1, text2);
+
+        //assert
+        result.Should().Be(expectedValue);
+    }
+}
